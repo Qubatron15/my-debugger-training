ecf29350484748663a4dd3a6d384b98f47581a8c
Kuba <qubatron15@gmail.com>
2024-10-13 14:23:00 +0200

    split to functions

--- anagrams.js
+++ anagrams.js
@@ -1,7 +1,5 @@
 // const randomized = realNames.map(name => name.replace(' ', '').toLowerCase().split('').sort(function(){return 0.5-Math.random()}).join(''))
 
-
-
 const realGivenNames = [
     'Weronika',
     'Karol',
@@ -43,26 +41,28 @@ const lettersRandomized = [
 ];
 
 const anagrams = [
-    "Wania Krosowe",          // skwearoownia (12 liter)
-    "Kornelia Wiśkows",       // woiśrnwaikiekls (15 liter)
-    "Kafa Gata",              // aągaktaf (8 liter)
-    "Baczek Jakitup",         // puitkkbaacej (12 liter)
-    "Bąk Marta Tusk",         // tmarazsutkeąb (13 liter)
-    "Kazimarka Czystałn",     // zinyralactaaakczmk (19 liter)
-    "Marta Zawosiodł",        // aioołzyawtsmzd (14 liter)
-    "Ziemi Grzegorńw",        // srgegzemriońwiiuz (18 liter)
-    "Zwyczaj Komaśstir",      // woamesrztuskyazżikn (20 liter)
-    "Grzegor Rusiakss"        // rzóieuriggksss (14 liter)
-];
+    "Ania Woskrewo",          // skwearoownia (12 liter)
+    "Wikal Irenosławsik",        // woiśrnwaikiekls (15 liter)
+    "Kafa Agat",              // aągaktaf (8 liter)
+    "Bajka Kupcięt",          // puitkkbaacej (12 liter)
+    "Marta Bużkaste",        // tmarazsutkeąb (13 liter)
+    "Kaczy Znalika Matarc",    // zinyralactaaakczmk (19 liter)
+    "Zosia Dławazotym",        // aioołzyawtsmzd (14 liter)
+    "Ziemig Urewizgrońs",        // srgegzemriońwiiuz (18 liter)
+    "Zamieszy Krukwotsazn",      // woamesrztuskyazżikn (20 liter)
+    "Grzesik Rógsuis"          // rzóieuriggksss (14 liter)
+  ];
 
 
-for (let i in anagrams) {
-    const lettersArray = anagrams[i].replace(' ', '').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").split('');
-    let result = lettersRandomized[i].toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+function checkChatGPT() {
+    for (let i in anagrams) {
+        const lettersArray = anagrams[i].replace(' ', '').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").split('');
+        let result = lettersRandomized[i].toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
-    lettersArray.forEach(letter => result = result.replace(letter, ''));
+        lettersArray.forEach(letter => result = result.replace(letter, ''));
 
-    console.log(`${anagrams[i]}: ${result}`);
+        console.log(`${anagrams[i]}: ${result}`);
+    }
 }
 
 
@@ -84,3 +84,5 @@ function checkAnagrams() {
         }
     }
 }
+
+checkAnagrams();
