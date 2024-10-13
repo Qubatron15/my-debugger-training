fea231725a196e705a1cb01036fd466f6e38177d
Kuba <qubatron15@gmail.com>
2024-10-13 14:54:31 +0200

    ok, i gave up - only 4 names are there. Issues forced to appear

--- anagrams.js
+++ anagrams.js
@@ -2,28 +2,16 @@
 
 const realGivenNames = [
     'Weronika',
-    'Karol',
     'Agata',
     'Jakub',
     'Mateusz',
-    'Natalia',
-    'Tomasz',
-    'Remigiusz',
-    'Mateusz',
-    'Sergiusz'
 ];
 
 const realSurnames = [
     'Sowa',
-    'Wiśniewski',
     'Fąk',
     'Patecki',
     'Trąbka',
-    'Kaczmarczyk',
-    'Działowy',
-    'Wierzgoń',
-    'Krzyżanowski',
-    'Górski'
 ];
 
 
@@ -40,28 +28,12 @@ const lettersRandomized = [
     'rzóieuriggksss'
 ];
 
-'skwearoownia',
-'woiśrnwaikiekls',
-'puitkkbaacej',
-'tmarazsutkeąb',
-'zinyralactaaakczmk',
-'aioołzyawtsmzd',
-'srgegzemriońwiiuz',
-'woamesrztuskyazżikn',
-'rzóieuriggksss'
-
 
 const anagrams = [
-    "Kafa Taag",             // aągaktaf (8 liter)
-    "Kasia Wronowe",         // skwearoownia (12 liter)
-    "Wiesław Kiornie",       // woiśrnwaikiekls (15 liter)
-    "Jakub Tacięk",          // puitkkbaacej (12 liter)
-    "Marta Kazusęt",         // tmarazsutkeąb (13 liter)
-    "Krzysztof Acanali",     // zinyralactaaakczmk (19 liter)
-    "Zosia Małowyta",        // aioołzyawtsmzd (14 liter)
-    "Mirek Zygrosien",       // srgegzemriońwiiuz (18 liter)
-    "Kazimierz Ustysko",     // woamesrztuskyazżikn (20 liter)
-    "Grzegorz Sukiś"         // rzóieuriggksss (14 liter)
+    "Kafa Taag",
+    "Kasia Wronowe",
+    "Jakub Kaciętp",
+    "Bartek Szamuta",
   ];
 
 
@@ -79,14 +51,27 @@ function checkChatGPT() {
 
 function checkAnagrams() {
     for (let fakeName of anagrams) {
-        const fakeNameNormalised = fakeName.replace(' ', '').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").split('').sort().join('');
-        // console.log(fakeNameNormalised);
+        const fakeNameNormalised = fakeName
+            .replace(' ', '')
+            .toLowerCase()
+            // .normalize("NFD")
+            // .replace(/[\u0300-\u036f]/g, "")
+            .split('')
+            .sort()
+            .join('');
 
         for (let realGiven of realGivenNames) {
             for (let realSurname of realSurnames) {
                 const combinationToTry = `${realGiven} ${realSurname}`;
 
-                const realNameNormalised = combinationToTry.replace(' ', '').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").split('').sort().join('');
+                const realNameNormalised = combinationToTry
+                    .replace(' ', '')
+                    .toLowerCase()
+                    // .normalize("NFD")
+                    // .replace(/[\u0300-\u036f]/g, "")
+                    .split('')
+                    .sort()
+                    .join('');
 
                 if (fakeNameNormalised === realNameNormalised) {
                     console.log(`${fakeName} is ${combinationToTry} // ${fakeNameNormalised}`);
@@ -96,4 +81,5 @@ function checkAnagrams() {
     }
 }
 
+// checkChatGPT();
 checkAnagrams();
