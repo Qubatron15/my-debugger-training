145e7deb6fe4fd2355f05a1f9f86e9835517a24c
Kuba <qubatron15@gmail.com>
2024-10-13 12:54:28 +0200

    real names + fake names + readme created

---
+++ README.md
@@ -0,0 +1,35 @@
+presentation:
+- setting up the debugger for simple app
+-- debugging in WebStorm
+-- debugging simple algorithms
+-- recognizing the anagrams
+
+- setting up the debugger for real project
+-- debugging in chrome
+-- debugging the real project frontend
+--- just simple items in the code
+-- debugging the real project backend
+--- --inspect flag
+--- another debugger somewhere in the code
+
+- debugging hacks
+-- triggering the debugger for tooltip
+-- adding the debugger with timeout
+-- adding the debugger with event listener
+-- debugger breakpoints in chrome
+-- just don't use Firefox
+
+
+
+
+testing names:
+Weronika Sowa
+Karol Wiśniewski
+Agata Fąk
+Jakub Patecki
+Mateusz Trąbka
+Natalia Kaczmarczyk
+Tomasz Działowy
+Remigiusz Wierzgoń
+Mateusz Krzyżanowski
+Sergiusz Górski
---
+++ anagrams.js
@@ -0,0 +1,39 @@
+// const randomized = realNames.map(name => name.replace(' ', '').toLowerCase().split('').sort(function(){return 0.5-Math.random()}).join(''))
+
+// skwearoownia
+// woiśrnwaikiekls
+// aągaktaf
+// puitkkbaacej
+// tmarazsutkeąb
+// zinyralactaaakczmk
+// aioołzyawtsmzd
+// srgegzemriońwiiuz
+// woamesrztuskyazżikn
+// rzóieuriggksss
+
+const realNames = [
+    'Weronika Sowa',
+    'Karol Wiśniewski',
+    'Agata Fąk',
+    'Jakub Patecki',
+    'Mateusz Trąbka',
+    'Natalia Kaczmarczyk',
+    'Tomasz Działowy',
+    'Remigiusz Wierzgoń',
+    'Mateusz Krzyżanowski',
+    'Sergiusz Górski'
+];
+
+const fakeNames = [
+    'Ariwona Skeow',
+    'Wikres Kilaniows',
+    'Afka Gataą',
+    'Jukba Pikateck',
+    'Batem Stukąrz',
+    'Karcma Zylkatinaz',
+    'Aztom Wiołyszd',
+    'Ziurw Gnieśmiroz',
+    'Zemswor Azikytskaż',
+    'Grisie Rózkszug'
+];
+
